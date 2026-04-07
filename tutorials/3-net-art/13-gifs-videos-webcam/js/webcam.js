66b819193a4e71e0dee21111d93c66690904b0a1
fyreflying <ethan.castaneda@sjsu.edu>
2026-04-07 09:41:46 -0700

    tutorial 13 additions

---
+++ tutorials/3-net-art/13-gifs-videos-webcam/js/webcam.js
@@ -0,0 +1,22 @@
+
+const webcamVideo = document.querySelectorAll("#webcam")
+
+async function startWebcam() {
+    
+    try {
+
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        })
+
+        webcamVideo.srcObject = stream;
+
+
+    } catch (error) {
+        console.log("error accessing webcam");
+    }
+
+}
+
+startWebcam();
